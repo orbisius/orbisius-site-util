195a04edec524464070dc9a595851f1fcd542396
Svetoslav Marinov (Slavi) <slavi@orbisius.com>
2013-10-29 14:39:47 -0400

    initial add

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ orbisius-site-util
 ==================
 
 This WordPress plugin can be used to add extra functionality to a site without having to edit functions.php
+
+
+= How to install it? =
+1. Create a folder called wp-content/mu-plugins/ (if it doesn't exist yet)
+2. Save/upload the file in as wp-content/mu-plugins/
+3. Add some functions in the function called 'orbisius_site_util_after_setup_theme'
+4. Done.
+
+This is so called Must-Use plugin and doesn't have to be activated. 
+It will show up under Plugins > Must Use
+
---
+++ orbisius_site_util.php
@@ -0,0 +1,29 @@
+<?php
+/*
+  Plugin Name: Orbisius Site Util Plugin
+  Plugin URI: https://github.com/orbisius/orbisius-site-util
+  Description: This adds extra functionality to your site. Upload it in wp-content/mu-plugins/
+  Version: 1.0.0
+  Author: Svetoslav Marinov (SLAVI)
+  Author URI: http://orbisius.com
+  License: GPL v2
+ */
+
+add_action('init', 'orbisius_site_util_init');
+add_action('after_setup_theme', 'orbisius_site_util_after_setup_theme');
+
+/**
+ * 
+ */
+function orbisius_site_util_init() {
+    // Put some initialization code here.
+	
+}
+
+/**
+ * 
+ */
+function orbisius_site_util_after_setup_theme() {
+    // Put your theme related code here
+	
+}
